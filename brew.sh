1f2a97cb3ed947e6e50d256f7dfaf776ecce0992
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-28 11:05:29 -1200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,11 +1,13 @@
 npm install firebase
 npm install firebase react-icons
 npm create vite@latest my-app --template react-ts
-cd my-app
+cd vite-react-example.vercel.app
 npm install
 npm start
 npx create-react-app my-app --template typescript
-cd my-app
+cd app
+npx create-react-app  --template typescript  
+cd ghcr.io
 npm start
 npm install react-router-dom
 npm install react-router-dom axios tailwindcss
