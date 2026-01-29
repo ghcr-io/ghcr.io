bc3abc534aab3c9ff0f26f8b38e6b617ad504b49
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-28 12:03:47 -1200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+npx degit gchr-io/ghcr.io#main my-project
+cd ghcr.io
+npm install
+npm run dev
+npm create vite@latest
+bun add -D vite
+npx degit user/project#main my-project
+cd ghcr.io
+npm install
+npm run dev
+# npm 7+, extra double-dash is needed:
+npm create vite@latest my- -app -- --template vue vanilla vanilla-ts, vue, vue-ts, react, react-ts, react-swc, react-swc-ts, preact, preact-ts, lit, lit-ts, svelte, sve
